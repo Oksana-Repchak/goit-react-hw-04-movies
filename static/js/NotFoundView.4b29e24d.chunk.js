e3f7fa07735d46a7f7ab7ea8d1eba9e5d1200c1d
Oksana Repchak <oksa.repchak@gmail.com>
2020-12-28 22:43:27 +0200

    Updates

---
+++ static/js/NotFoundView.4b29e24d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[3],{72:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return i}));var c=n(1);function i(){return Object(c.jsx)(c.Fragment,{children:Object(c.jsx)("h1",{children:"404 Page not found :("})})}}}]);
+//# sourceMappingURL=NotFoundView.4b29e24d.chunk.js.map
