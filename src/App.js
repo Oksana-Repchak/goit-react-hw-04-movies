c798fd552eed06624375f4b02c855605aff36607
Oksana Repchak <oksa.repchak@gmail.com>
2020-12-28 22:44:56 +0200

    add React.lazy

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
+import { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Container from './components/Container';
 import AppBar from './components/AppBar';
+import Loader from './components/Loader';
 
-import HomePageView from './views/HomePageView';
-import MoviesPageView from './views/MoviesPageView';
-import NotFoundView from './views/NotFoundView';
-import MovieDetailsPageView from './views/MovieDetailsPageView';
+const HomePageView = lazy(() =>
+  import('./views/HomePageView' /* webpackChunkName: "HomePageView" */),
+);
+const MoviesPageView = lazy(() =>
+  import('./views/MoviesPageView' /* webpackChunkName: "MoviesPageView" */),
+);
+const NotFoundView = lazy(() =>
+  import('./views/NotFoundView' /* webpackChunkName: "NotFoundView" */),
+);
+const MovieDetailsPageView = lazy(() =>
+  import(
+    './views/MovieDetailsPageView' /* webpackChunkName: "MovieDetailsPageView" */
+  ),
+);
 
 function App() {
   return (
     <Container>
       <AppBar />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePageView />
+          </Route>
 
-      <Switch>
-        <Route path="/" exact>
-          <HomePageView />
-        </Route>
+          <Route path="/movies" exact>
+            <MoviesPageView />
+          </Route>
 
-        <Route path="/movies" exact>
-          <MoviesPageView />
-        </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPageView />
+          </Route>
 
-        <Route path="/movies/:movieId">
-          <MovieDetailsPageView />
-        </Route>
-
-        <Route>
-          <NotFoundView />
-        </Route>
-      </Switch>
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </Suspense>
     </Container>
   );
 }
